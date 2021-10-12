2e4046a2b08b83e9c5df3d20397f42422bdc0bb5
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2021-10-12 00:49:18 -0300

    Adição da entidade configuração de estoque de produto

---
+++ src/main/java/com/almox/model/entidades/ConfiguracaoEstoqueProduto.java
@@ -0,0 +1,51 @@
+package com.almox.model.entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cep_configuracao_estoque_produto")
+public class ConfiguracaoEstoqueProduto extends Auditavel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cep_id")
+    private Long id;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "{ConfiguracaoEstoqueProduto.estoqueMinimo.DecimalMin}")
+    @NotNull(message = "{ConfiguracaoEstoqueProduto.estoquemin.notnull}")
+    @Column(name = "cep_estoque_min", nullable = false)
+    private BigDecimal estoqueMinimo;
+
+    @Column(name = "cep_estoque_atual", nullable = false)
+    private BigDecimal estoqueAtual;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "{ConfiguracaoEstoqueProduto.estoqueMaximo.DecimalMin}")
+    @NotNull(message = "{ConfiguracaoEstoqueProduto.estoqueMaximo.NotNull}")
+    @Column(name = "cep_estoque_max", nullable = false)
+    private BigDecimal estoqueMaximo;
+
+    @NotNull(message = "{ConfiguracaoEstoqueProduto.controlaEstoqueMinimo.NotNull}")
+    @Column(name = "cep_controla_estoque_min", nullable = false)
+    private Boolean controlaEstoqueMinimo;
+
+    @NotNull(message = "{ConfiguracaoEstoqueProduto.controlaEstoqueMaximo.NotNull}")
+    @Column(name = "cep_controla_estoque_max", nullable = false)
+    private Boolean controlaEstoqueMaximo;
+}
